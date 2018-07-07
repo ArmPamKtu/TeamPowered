8b319a686b7653293677b21b6716e50ef215ba5b
DESKTOP-7U407JF\User <arminas.pamakstis@ktu.edu>
2018-07-07 14:49:34 +0300

    Split name into first and last name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Namee { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
